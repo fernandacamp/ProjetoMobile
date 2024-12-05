338b0692ea5f8ab39f1b96f8ae49475a68bbb277
jessicapachecoj <jessicapacheco258@gmail.com>
2024-12-05 15:18:38 -0300

    Configuração do Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; 
 import 'package:projeto_mobile/screens/login_page.dart';
 import 'package:projeto_mobile/settings/routes.dart';
 import 'screens/menu_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  
+  await Firebase.initializeApp();  
   runApp(MyApp());
 }
 
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       initialRoute: AppRoutes.login,
-      routes : AppRoutes.routes,
+      routes: AppRoutes.routes,
     );
   }
 }
